393dc58bc44a8f6733c08dc14f0cac4cabbacb2d
Sazhin-Kostya <sazhinkostya@gmail.com>
2023-06-30 23:10:01 +0300

    Закончил дз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+console.log('kjkjkj')
+
+const getCarrentTime = function (carrentTime) {
+    const seconds = carrentTime.seconds;
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds))
+    
+};
+player.on('timeupdate', throttle(getCarrentTime, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+
+
+const form = document.querySelector('.feedback-form')
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const STORAGE_KEY = "feedback-form-state";
+
+
+form.addEventListener('submit', onFormSubmit );
+email.addEventListener('input', throttle(onFormInput, 500));
+message.addEventListener('input', throttle(onFormInput, 500))
+populateForm();
+
+
+function onFormInput(event) {
+    const formData = {
+        email: email.value,
+        message: message.value,
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+
+function onFormSubmit(event) {
+    event.preventDefault();  
+    const consoleObj = localStorage.getItem(STORAGE_KEY);
+    const parceConsoleMsg = JSON.parse(consoleObj)
+    if (parceConsoleMsg) {
+        console.log(parceConsoleMsg);
+    }  
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+};
+
+
+function populateForm() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    const parceSaveMess = JSON.parse(saveMessage);
+    if (parceSaveMess) {
+        email.value = parceSaveMess.email;
+        message.value = parceSaveMess.message
+    }
+};
